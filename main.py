08778586c2878c14ea3b79a42150083d6767a44e
krystalmm <taeyeon_aina@live.jp>
2021-03-12 23:47:19 +0900

    [add] while

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import roboter.utils
 
 
+# import roboter.utils
+
+# while True:
+#     name = input('こんにちは！私はRobokoです。あなたの名前は何ですか？\n')
+#     if name is not None:
+#         input('{}さん。どこのレストランが好きですか？'.format(name))
+
+
+while True:
+    word = input('Enter:')
+    if word == 'ok':
+        break
+    print('next')
